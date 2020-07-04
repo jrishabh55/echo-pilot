c1806a30c526752fd72a78d7b5381f2d72bba9bc
Rishabh Jain <rishabh@codeation.io>
2020-07-05 01:08:19 +0530

    chore: removed comments

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-/**
- * Required External Modules
- */
-
 import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
@@ -12,10 +8,6 @@ import router from './routes';
 
 dotenv.config();
 
-/**
- * App Variables
- */
-
 if (!process.env.PORT) {
   console.error('PORT not defined.');
   process.exit(1);
@@ -25,20 +17,12 @@ const PORT: number = parseInt(process.env.PORT as string, 10);
 
 const app = express();
 
-/**
- *  App Configuration
- */
-
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(morgan('combined'));
 
 app.use(router);
-
-/**
- * Server Activation
- */
 
 const server = app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
