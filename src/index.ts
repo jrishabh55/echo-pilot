7d6f98918ebf536865e2c7c3c22c7dc91063d9f9
Rishabh Jain <rishabh@codeation.io>
2020-07-05 00:25:43 +0530

    feat: added morgan

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import morgan from "morgan";
 
 dotenv.config();
 
@@ -29,6 +30,7 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(morgan('combined'));
 
 /**
  * Server Activation
