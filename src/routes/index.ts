222cf122f93577962638cd30e6f3f4c9ff5e505d
Rishabh Jain <rishabh@codeation.io>
2020-07-06 15:34:07 +0530

    feat: added channel routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,20 @@
 import { Router } from 'express';
+import { youtube, oAuth2 } from '../providers/google.provider';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.json({ data: 'reached' });
-})
+router.post('/auth/youtube/channels', async (req, res) => {
+  const oAuthClient = oAuth2();
+
+  oAuthClient.setCredentials(req.body);
+
+  const channels = await youtube.channels.list({
+    auth: oAuthClient,
+    part: ['snippet','contentDetails', 'statistics'],
+    mine: true,
+  });
+
+  res.json({ channels: channels.data });
+});
 
 export default router;
